ef23816fd933636f90d95c8fd4b53f0b918b3f73
Dubkov <yevgeniydubkov@gmail.com>
2022-05-19 23:31:16 +0300

    done

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,7 @@ from todo.views import ProjectModelViewSet, TodoModelViewSet
 from rest_framework.authtoken import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from graphene_django.views import GraphQLView
 from rest_framework import permissions
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -47,4 +48,5 @@ urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
 ]
